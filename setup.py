d0ff43f92ddf058b6f49ce86def0f6b0fc2efcf6
Prahlad Yeri <prahladyeri@yahoo.com>
2019-07-14 14:34:31 +0530

    build: released 1.0.3

--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ import os
 from siterank import __version__, __description__, __author__, __email__, __license__
 from setuptools import setup, find_packages
 
+pkg_name = 'siterank'
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 s = setup(
-	name='siterank',
+	name=pkg_name,
 	version=__version__,
 	license=__license__,
 	description=__description__,
 	long_description=read("README.md"),
 	long_description_content_type='text/markdown',
 	keywords="internet",
-	url='https://github.com/prahladyeri/siterank',
+	url='https://github.com/prahladyeri/' + pkg_name,
 	packages=find_packages(),
 	include_package_data=True,
 	entry_points={
