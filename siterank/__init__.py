621e7bb2035f87e7e75054ed78163bcc065cd8fb
Prahlad Yeri <prahladyeri@yahoo.com>
2022-02-16 11:58:59 +0530

    build: released 1.0.7

--- siterank/__init__.py
+++ siterank/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Prahlad Yeri"
 __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 __title__ = "siterank"
 __description__ = "get alexa global rank for a given website or domain"
 __license__ = "MIT"
