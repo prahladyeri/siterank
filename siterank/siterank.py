9845ad462acbdef187cc938dba1e64d32ff736e2
Prahlad Yeri <prahladyeri@yahoo.com>
2022-02-16 11:07:07 +0530

    feat: added local caching

--- siterank/siterank.py
+++ siterank/siterank.py
@@ -5,12 +5,30 @@
 #
 # @author Prahlad Yeri
 #
-import time
+import time, pickle, sysconfig
 import urllib.request
 import sys, os
 import argparse
 import xml.etree.ElementTree as ET
 from siterank import __title__, __version__
+
+#@todo: move this to a common util library
+def get_install_path():
+    tpath = sysconfig.get_path('purelib') + os.sep + "siterank"
+    if not os.path.exists(tpath):
+        os.makedirs(tpath)
+    return tpath
+
+# check/load local cache
+pkl_path = get_install_path() + os.sep + "siterank.pkl"
+if os.path.exists(pkl_file):
+    cache = pickle.load(open(pkl_path, 'rb'))
+    print("successfully imported cache..")
+else:
+    cache = {}
+    pickle.dump(cache, open(pkl_file, 'wb'), -1) 
+    print("cache doesn't exist, created cache..")
+
 
 def split_dict(data, size=10):
     #s = {}
@@ -26,25 +44,31 @@ def split_dict(data, size=10):
         s.append(dct)
     return s
 
-def get_ranks(url_list):
+def get_ranks(url_list, refresh=False):
     ranks = {}
     #for url in url_list:
     cnt = len(url_list)
     for i in range(cnt):
         url = url_list[i]
-        turl = "http://data.alexa.com/data?cli=10&url=" + url
-        req = urllib.request.Request(turl)
-        with urllib.request.urlopen(req) as fp:
-            output = fp.read().decode('utf-8')
-            tree = ET.ElementTree(ET.fromstring(output))
-            root = tree.getroot()
-            sd = root.find("SD")
-            if sd != None:
-                rnk = int(sd.find("POPULARITY").get("TEXT"))
-                ranks[url] = rnk
-            else:
-                #not_found.append(url)
-                ranks[url] = -1
+        if not refresh:
+            # check local cache, if found, no need of requesting
+            if url in cache: # found
+                ranks[url] = cache[url]
+        if url not in ranks:
+            turl = "http://data.alexa.com/data?cli=10&url=" + url
+            req = urllib.request.Request(turl)
+            with urllib.request.urlopen(req) as fp:
+                output = fp.read().decode('utf-8')
+                tree = ET.ElementTree(ET.fromstring(output))
+                root = tree.getroot()
+                sd = root.find("SD")
+                if sd != None:
+                    rnk = int(sd.find("POPULARITY").get("TEXT"))
+                    ranks[url] = rnk
+                else:
+                    #not_found.append(url)
+                    ranks[url] = -1
+                cache[url] = ranks[url] # also save to cache
         ss = "%d/%d. %s" % (i+1,cnt, url.ljust(100))
         print(ss, end='\r', flush=True)
         if i%30 == 0:
