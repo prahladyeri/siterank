539ef2a4c67e820414e444328c39968072771655
Prahlad Yeri <prahladyeri@yahoo.com>
2022-02-16 11:56:57 +0530

    fix: pickling

--- siterank/siterank.py
+++ siterank/siterank.py
@@ -20,13 +20,14 @@ def get_install_path():
     return tpath
 
 # check/load local cache
+cache = {}
 pkl_path = get_install_path() + os.sep + "siterank.pkl"
-if os.path.exists(pkl_file):
+if os.path.exists(pkl_path):
     cache = pickle.load(open(pkl_path, 'rb'))
-    print("successfully imported cache..")
+    print("successfully imported cache. %d records found.." % len(cache.keys()))
 else:
     cache = {}
-    pickle.dump(cache, open(pkl_file, 'wb'), -1) 
+    pickle.dump(cache, open(pkl_path, 'wb'), -1) 
     print("cache doesn't exist, created cache..")
 
 
@@ -53,8 +54,10 @@ def get_ranks(url_list, refresh=False):
         if not refresh:
             # check local cache, if found, no need of requesting
             if url in cache: # found
+                print("found in cache:", url)
                 ranks[url] = cache[url]
         if url not in ranks:
+            print("fetching live:", url)
             turl = "http://data.alexa.com/data?cli=10&url=" + url
             req = urllib.request.Request(turl)
             with urllib.request.urlopen(req) as fp:
@@ -69,9 +72,10 @@ def get_ranks(url_list, refresh=False):
                     #not_found.append(url)
                     ranks[url] = -1
                 cache[url] = ranks[url] # also save to cache
+                pickle.dump(cache, open(pkl_path,'wb'), -1)
         ss = "%d/%d. %s" % (i+1,cnt, url.ljust(100))
         print(ss, end='\r', flush=True)
-        if i%30 == 0:
+        if i>0 and i%30 == 0:
             #print(i, 'wait')
             time.sleep(3)
     print("")
@@ -83,10 +87,11 @@ def main():
         return
     parser = argparse.ArgumentParser()
     parser.add_argument('list', nargs='+', default=[], help='List of URLs EX: www.google.com www.yahoo.com etc.')
+    parser.add_argument('-r', '--refresh', help='Refresh cache', action='store_true')
     parser.add_argument('-v', '--version', help='Display Version', action='store_true')
     args = parser.parse_args()
     
-    ranks = get_ranks(args.list)
+    ranks = get_ranks(args.list, args.refresh)
     sranks = [(k,ranks[k] if ranks[k]<1000 else str(int(ranks[k]/1000)) + "k") for k in sorted(ranks, key=ranks.get, reverse=False)]
     
     col_len = 40
